86a1999a11bb9ec8a64bfb058db9cde14dd6cc03
Edoardo Paggi <edoardo.paggi.1997@gmail.com>
2026-05-24 16:38:43 +0200

    Create two new example files

---
+++ c-programming/c-programming-a-modern-approach-kim-n-king/chapter-02/examples/dweight2.c
@@ -0,0 +1,26 @@
+/*
+ * Name: dweight2.c
+ * Author: Edoardo Paggi
+ * Purpose: calculate the dimensional weight of a package from user input.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+    int height, length, width, volume, weight;
+
+    printf("Enter height of box: ");
+    scanf("%d", &height);
+    printf("Enter length of box: ");
+    scanf("%d", &length);
+    printf("Enter width of box: ");
+    scanf("%d", &width);
+    volume = height * length * width;
+    weight = (volume + 165) / 166;
+
+    printf("Volume (cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d\n", weight);
+
+    return 0;
+}
