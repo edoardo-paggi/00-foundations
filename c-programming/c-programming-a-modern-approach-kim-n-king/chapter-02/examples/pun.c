f7d528689471b988dceab97f29030bdbda420ddc
Edoardo Paggi <edoardo.paggi.1997@gmail.com>
2026-05-24 16:19:42 +0200

    Update pun.c with comments
    Recompile pun.c in ./build directory

--- c-programming/c-programming-a-modern-approach-kim-n-king/chapter-02/examples/pun.c
+++ c-programming/c-programming-a-modern-approach-kim-n-king/chapter-02/examples/pun.c
@@ -1,6 +1,14 @@
+/*
+ * Name: pun.c
+ * Author: Edoardo Paggi
+ * Purpose: print the bad pun.
+ */
+
 #include <stdio.h>
 
 int main(void) {
+
     printf("To C, or not to C: that is the question.\n");
+
     return 0;
 }
