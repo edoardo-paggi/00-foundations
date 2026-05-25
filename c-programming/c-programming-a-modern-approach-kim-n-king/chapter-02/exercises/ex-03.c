d93c94dea92024c93414a7a2516812b26fd39d56
Edoardo Paggi <edoardo.paggi.1997@gmail.com>
2026-05-25 19:39:42 +0200

    Create and build ex-03

---
+++ c-programming/c-programming-a-modern-approach-kim-n-king/chapter-02/exercises/ex-03.c
@@ -0,0 +1,15 @@
+/*
+ * Name: ex-03.c
+ * Author: Edoardo Paggi
+ * Task: modify programming project 2 so that it prompts the user to enter the
+         radius of the sphere.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Hello, ex-03\n");
+
+    return 0;
+}
