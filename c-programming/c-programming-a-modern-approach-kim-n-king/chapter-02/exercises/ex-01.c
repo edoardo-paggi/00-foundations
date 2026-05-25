5d936444abb6ab3454a241a737372763b233e6aa
Edoardo Paggi <edoardo.paggi.1997@gmail.com>
2026-05-25 19:02:43 +0200

    Refactor chapter-02
    Create exercises directory.
    Create and build first exercise file.

---
+++ c-programming/c-programming-a-modern-approach-kim-n-king/chapter-02/exercises/ex-01.c
@@ -0,0 +1,21 @@
+/*
+ * Name: ex-01.c
+ * Author: Edoardo Paggi
+ * Task: write a program that uses printf to print the following image to the
+ screen.
+
+         *
+       *
+     *
+   * * * * *
+
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Hello, ex-01\n");
+
+    return 0;
+}
