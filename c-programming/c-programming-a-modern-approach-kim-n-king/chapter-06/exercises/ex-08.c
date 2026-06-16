b3955d2c3c0c8769e79956cd7a20dd30e0d1557a
Edoardo Paggi <edoardo.paggi.1997@gmail.com>
2026-06-16 16:28:43 +0200

    Complete chapter-08 ex-08

---
+++ c-programming/c-programming-a-modern-approach-kim-n-king/chapter-06/exercises/ex-08.c
@@ -0,0 +1,25 @@
+/* 
+ * Name: ex-08.c
+ * Author: Edoardo Paggi
+ * Task: write a program that prints a calendar for a month. The user must 
+         specify the number of days in the month and the day of the week on which the month begins:
+         
+         Enter number of days in month: 31
+         Enter starting day of the week (1=Sun, 7=Sat): 3
+         
+                      1     2     3     4     5
+          6     7     8     9    10    11    12
+         13    14    15    16    17    18    19
+         20    21    22    23    24    25    26
+         27    28    29    30    31
+         
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Hello, cal\n");
+
+    return 0;
+}
