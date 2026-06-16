850d59a7f67678df1862453a7d3b876807957758
Edoardo Paggi <edoardo.paggi.1997@gmail.com>
2026-06-16 16:55:10 +0200

    Complete chapter-06 ex-08

--- c-programming/c-programming-a-modern-approach-kim-n-king/chapter-06/exercises/ex-08.c
+++ c-programming/c-programming-a-modern-approach-kim-n-king/chapter-06/exercises/ex-08.c
@@ -19,7 +19,28 @@
 
 int main(void) {
 
-    printf("Hello, cal\n");
+    int number_days, day_week, last_day;
+
+    printf("\nEnter number of days in month: ");
+    scanf("%d", &number_days);
+    printf("Enter starting day of the week (1=Sun, 7=Sat): ");
+    scanf("%d", &day_week);
+
+    last_day = 7 - day_week + 1;
+
+    // First line
+    for (int j = 1; j < day_week; j++)
+        printf("  \t");
+
+    // Solve
+    for (int i = 1; i <= number_days; i++) {
+        printf("%2d\t", i);
+
+        if (i == last_day || i == last_day + 7 || i == last_day + 14 ||
+            i == last_day + 21 || i == last_day + 28)
+            printf("\n");
+    }
+    printf("\n");
 
     return 0;
 }
