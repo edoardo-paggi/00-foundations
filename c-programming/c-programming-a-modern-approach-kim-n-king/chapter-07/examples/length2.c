387c3d0652131f3a1f995c6fce0b27fb0328e64c
Edoardo Paggi <edoardo.paggi.1997@gmail.com>
2026-06-18 15:02:10 +0200

    Rewrite chapter-07 examples

---
+++ c-programming/c-programming-a-modern-approach-kim-n-king/chapter-07/examples/length2.c
@@ -0,0 +1,21 @@
+/*
+ * Name: length2.c
+ * Author: Edoardo Paggi
+ * Purpose: determines the length of a message
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+    int len = 0;
+
+    printf("Enter a message: ");
+
+    while (getchar() != '\n')
+        len++;
+
+    printf("Your message was %d character(s) long.\n", len);
+
+    return 0;
+}
