52e5d0564df45d2c285fd9cee49cb2a956a02ca1
Edoardo Paggi <edoardo.paggi.1997@gmail.com>
2026-07-02 15:11:58 +0200

    Rewrite chapter-11 examples

---
+++ c-programming/c-programming-a-modern-approach-kim-n-king/chapter-11-pointers/examples/maxmin.c
@@ -0,0 +1,37 @@
+/*
+ * Name: maxminc.
+ * Author: Edoardo Paggi
+ * Purpose: find the maximum and minimum in a vector
+*/
+
+#include <stdio.h>
+
+#define N 10
+
+void max_min(int a[], int n, int *max, int *min);
+
+int main(void) {
+    int b[N];
+    int big, small;
+
+    printf("Enter %d numbers: ", N);
+    for (int i = 0; i < N; i++)
+        scanf("%d", &b[i]);
+
+    max_min(b, N, &big, &small);
+
+    printf("Largest: %d\n", big);
+    printf("Smallest: %d\n", small);
+
+    return 0;
+}
+
+void max_min(int a[], int n, int *max, int *min) {
+    *max = *min = a[0];
+
+    for (int i = 0; i < N; i++)
+        if (a[i] > *max)
+            *max = a[i];
+        else if (a[i] < *min)
+            *min = a[i];
+}
