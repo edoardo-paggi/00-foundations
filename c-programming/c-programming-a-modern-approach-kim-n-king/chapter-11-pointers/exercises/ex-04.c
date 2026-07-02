206cd4f6303ffe40f6c5ac68a8901e056c147bf0
Edoardo Paggi <edoardo.paggi.1997@gmail.com>
2026-07-02 16:34:36 +0200

    Complete chapter-11 ex-04

---
+++ c-programming/c-programming-a-modern-approach-kim-n-king/chapter-11-pointers/exercises/ex-04.c
@@ -0,0 +1,163 @@
+/*
+ * Name: ex-04.c
+ * Author: Edoardo Paggi
+ * Task: modify the poker.c program by moving all external variables into the 
+         main program and modifying the functions so they communicate through argument passing. The analyze_hand function needs to modify the straight, flush, four, three, and pairs variables, so it must be passed pointers to them.
+*/ 
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_RANKS 13
+#define NUM_SUITS 4
+#define NUM_CARDS 5
+
+void read_cards(int num_rank[], int num_suit[]);
+void analyze_hand(bool *royal_straight, bool *straight, bool *flush,
+                  bool *four, bool *three, int *pairs, int num_rank[], 
+                  int num_suit[]);
+void print_result(bool *royal_straight, bool *straight, bool *flush,
+                  bool *four, bool *three, int *pairs);
+
+int main(void) {
+    int num_in_rank[NUM_RANKS];
+    int num_in_suit[NUM_SUITS];
+    bool royal_straight, straight, flush, four, three;
+    int pairs;
+
+    for (;;) {
+        read_cards(num_in_rank, num_in_suit);
+
+        analyze_hand(&royal_straight, &straight, &flush, &four, &three, &pairs,
+                     num_in_rank, num_in_suit);
+                     
+        print_result(&royal_straight, &straight, &flush, &four, &three, &pairs);
+    }
+}
+
+void read_cards(int num_rank[], int num_suit[]) {
+    bool card_exists[NUM_RANKS][NUM_SUITS];
+    bool bad_card;
+    char ch, rank_ch, suit_ch;
+    int rank, suit;
+    int cards_read = 0;
+
+    for (rank = 0; rank < NUM_RANKS; rank++) {
+        num_rank[rank] = 0;
+        for (suit = 0; suit < NUM_SUITS; suit++)
+            card_exists[rank][suit] = false;
+    }
+
+    for (suit = 0; suit < NUM_SUITS; suit++)
+        num_suit[suit] = 0;
+    
+    while (cards_read < NUM_CARDS) {
+        bad_card = false;
+
+        printf("Enter a card: ");
+
+        rank_ch = getchar();
+        switch (rank_ch) {
+            case '0':           exit(EXIT_SUCCESS);          
+            case '2':           rank = 0;  break;
+            case '3':           rank = 1;  break;
+            case '4':           rank = 2;  break;
+            case '5':           rank = 3;  break;
+            case '6':           rank = 4;  break;
+            case '7':           rank = 5;  break;
+            case '8':           rank = 6;  break;
+            case '9':           rank = 7;  break;
+            case 't': case 'T': rank = 8;  break;
+            case 'j': case 'J': rank = 9;  break;
+            case 'q': case 'Q': rank = 10; break;
+            case 'k': case 'K': rank = 11; break;
+            case 'a': case 'A': rank = 12; break;
+            default:            bad_card = true;
+        }
+
+        suit_ch = getchar();
+        switch (suit_ch) {
+            case 'c': case 'C': suit = 0; break;
+            case 'd': case 'D': suit = 1; break;
+            case 'h': case 'H': suit = 2; break;
+            case 's': case 'S': suit = 3; break;
+            default:            bad_card = true;
+        }
+        
+        while ((ch = getchar()) != '\n')
+            if (ch != ' ') bad_card = true;
+
+        if (bad_card)
+            printf("Bad card; ignored.\n");
+        else if (card_exists[rank][suit])
+            printf("Duplicate card; ignored.\n");
+        else {
+            num_rank[rank]++;
+            num_suit[suit]++;
+            card_exists[rank][suit] = true;
+            cards_read++;
+        }
+    }
+}
+
+void analyze_hand(bool *royal_straight, bool *straight, bool *flush,
+                  bool *four, bool *three, int *pairs, int num_rank[], 
+                  int num_suit[]) {
+    int num_consec = 0;
+    int rank, suit;
+    *royal_straight = false;
+    *straight = false;
+    *flush = false;
+    *four = false;
+    *three = false;
+    *pairs = 0;
+
+    /* check if it's a flush */
+    for (suit = 0; suit < NUM_SUITS; suit++)
+        if (num_suit[suit] == NUM_CARDS)
+            *flush = true;
+
+    /* check if it's a straight */
+    rank = 0;
+    while (num_rank[rank] == 0) rank++;
+
+    if (rank == 8) {
+        *royal_straight = true;
+        return;
+    } else if (num_rank[0] && num_rank[1] && num_rank[2] && num_rank[3] &&  
+               num_rank[12]) {
+        *straight = true;
+        return;            
+    } else {
+        for (; rank < NUM_RANKS && num_rank[rank] > 0; rank++)
+            num_consec++;
+        if (num_consec == NUM_CARDS) {
+            *straight = true;
+            return;
+        }
+    }    
+
+    /* checks for poker, three of a kind and pairs */
+    for (rank = 0; rank < NUM_RANKS; rank++) {
+        if (num_rank[rank] == 4) *four = true;
+        if (num_rank[rank] == 3) *three = true;
+        if (num_rank[rank] == 2) *pairs++;
+    }
+}
+
+void print_result(bool *royal_straight, bool *straight, bool *flush,
+                  bool *four, bool *three, int *pairs) {
+    if (*royal_straight && *flush)  printf("Royal flush");
+    else if (*straight && *flush)   printf("Straight flush");
+    else if (*four)                 printf("Four of a kind");
+    else if (*three && *pairs == 1) printf("Full house");
+    else if (*flush)                printf("Flush");
+    else if (*straight)             printf("Straight");
+    else if (*three)                printf("Three of a kind");
+    else if (*pairs == 2)           printf("Two pairs");
+    else if (*pairs == 1)           printf("Pair");
+    else                            printf("High card");
+
+    printf("\n\n");
+}
